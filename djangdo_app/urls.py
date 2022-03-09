49ebe2afa5287dd21de8146bac903e002cddb9d6
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-09 15:32:28 -0800

    Add ItemListView

--- djangdo_app/urls.py
+++ djangdo_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.ListListView.as_view(), name="index"),
+    path("",
+         views.ListListView.as_view(), name="index"),
+    path("list/<int:list_id>/",
+         views.ItemListView.as_view(), name="list"),
 ]
