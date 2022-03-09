3f574bc8656b2eb87f5b8931526980c27c20e153
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-09 14:51:52 -0800

    Create ItemListView

--- djangdo_app/views.py
+++ djangdo_app/views.py
@@ -1,7 +1,20 @@
 from django.views.generic import ListView
-from .models import ToDoList
+from .models import ToDoList, ToDoItem
 
 
 class ListListView(ListView):
     model = ToDoList
     template_name = "djangdo_app/index.html"
+
+
+class ItemListView(ListView):
+    model = ToDoItem
+    template_name = "djangdo_app/todo_list.html"
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["todo_list"] = ToDoList.objects.get(id=self.kwargs["list_id"])
+        return context
