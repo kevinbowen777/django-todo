6e05132509a836482a73ceb9d004366673bc3e71
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-09 17:13:21 -0800

    Define routes for ListDelete and ItemDelete

--- djangdo_app/urls.py
+++ djangdo_app/urls.py
@@ -9,8 +9,12 @@ urlpatterns = [
          views.ItemListView.as_view(), name="list"),
     path("list/add/",
          views.ListCreate.as_view(), name="list-add"),
+    path("list/<int:pk>/delete/",
+         views.ListDelete.as_view(), name="list-delete"),
     path("list/<int:list_id>/item/add/",
          views.ItemCreate.as_view(), name="item-add"),
     path("list/<int:list_id>/item/<int:pk>/",
          views.ItemUpdate.as_view(), name="item-update"),
+    path("list/<int:list_id>/item/<int:pk>/delete/",
+         views.ItemDelete.as_view(), name="item-delete"),
 ]
