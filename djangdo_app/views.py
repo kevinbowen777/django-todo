8d92790bf7c1c7985888246fde30045951022476
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-09 16:58:10 -0800

    ListDelete ItemDelete classes

--- djangdo_app/views.py
+++ djangdo_app/views.py
@@ -1,5 +1,10 @@
-from django.urls import reverse
-from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import ToDoList, ToDoItem
 
 
@@ -74,3 +79,20 @@ class ItemUpdate(UpdateView):
 
     def get_success_url(self):
         return reverse("list", args=[self.object.todo_list_id])
+
+
+class ListDelete(DeleteView):
+    model = ToDoList
+    success_url = reverse_lazy("index")
+
+
+class ItemDelete(DeleteView):
+    model = ToDoItem
+
+    def get_success_url(self):
+        return reverse_lazy("list", args=[self.kwargs["list_id"]])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todo_list"] = self.object.todo_list
+        return context
