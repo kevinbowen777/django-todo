23ad4f76794c1ecbd38a788a0ba9c53f5e6a25fb
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-30 22:10:15 -0700

    Add jumbotron to home. Update login/logout redirects

--- djangdo/urls.py
+++ djangdo/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ListListView.as_view(), name="home"),
+    path("list/", views.ListListView.as_view(), name="index"),
     path("about/", views.AboutPageView.as_view(), name="about"),
+    path("", views.HomePageView.as_view(), name="home"),
     path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
     path("list/add/", views.ListCreate.as_view(), name="list-add"),
     path("list/<int:pk>/delete/", views.ListDelete.as_view(), name="list-delete"),
