9cb88600e019c54f638f5a91619efb5e8de27d9c
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-30 19:57:54 -0700

    Write tests for signup page

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 
 class CustomUserTests(TestCase):
@@ -28,3 +29,25 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignupTests(TestCase):
+
+    username = "newuser"
+    email = "newuser@example.com"
+
+    def setUp(self):
+        url = reverse("signup")
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "registration/signup.html")
+        self.assertContains(self.response, "Sign Up")
+        self.assertNotContains(self.response, "This does not belong here.")
+
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(self.username, self.email)  # noqa: F841
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
