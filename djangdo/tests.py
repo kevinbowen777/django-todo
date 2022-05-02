3098bcbec3088a67deaf4d2beca27f45b1bd1008
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-02 12:04:27 -0700

    Create AboutPage tests

--- djangdo/tests.py
+++ djangdo/tests.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model  # noqa:F401
 from django.test import SimpleTestCase
 from django.urls import resolve, reverse
 
-from .views import HomePageView
+from .views import AboutPageView, HomePageView
 
 
 class HomePageTests(SimpleTestCase):
@@ -29,3 +29,25 @@ class HomePageTests(SimpleTestCase):
     def test_homepage_url_resolves_homepageview(self):
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("about")
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, "about.html")
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, "About Page")
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "Home Page")
+
+    def test_about_page_url_resolves_aboutpageview(self):
+        view = resolve("/about/")
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
