484f9314cf32464707a2d5333b6a1e25a119c4f7
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-02 11:27:00 -0700

    Create HomePage tests

--- djangdo/tests.py
+++ djangdo/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase  # noqa: F401
+from django.contrib.auth import get_user_model  # noqa:F401
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
 
-# Create your tests here.
+from .views import HomePageView
+
+
+class HomePageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
+
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, "Track your tasks")
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
