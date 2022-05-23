15560f6af126e6677f6bf7a89ed6321c9d80ac21
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-23 16:42:58 -0700

    Rename db & move DATABASE_URL to env

--- config/settings.py
+++ config/settings.py
@@ -73,13 +73,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
+DATABASES = {"default": env.dj_db_url("DATABASE_URL", default="postgres://postgres@db/postgres")}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
