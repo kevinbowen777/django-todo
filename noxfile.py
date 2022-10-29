50ce12b859ff34dd76f3870a789a81e45bf9408c
Kevin Bowen <kevin.bowen@gmail.com>
2022-10-28 19:43:54 -0700

    Update noxfile & adjust import ordering. Resolves #133

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,4 @@
-"""Nox sessions - djangdo."""
+"""Nox sessions - django-todo."""
 import tempfile
 
 import nox
@@ -27,13 +27,14 @@ def install_with_constraints(session, *args, **kwargs):
         session.run(
             "poetry",
             "export",
-            "--dev",
+            "--with",
+            "dev",
             "--format=requirements.txt",
             "--without-hashes",
             f"--output={requirements.name}",
             external=True,
         )
-        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+        session.install(f"--requirement={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=["3.11", "3.10", "3.9"])
@@ -74,7 +75,8 @@ def safety(session):
         session.run(
             "poetry",
             "export",
-            "--dev",
+            "--with",
+            "dev",
             "--format=requirements.txt",
             "--without-hashes",
             f"--output={requirements.name}",
