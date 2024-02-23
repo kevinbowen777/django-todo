cba3d9be3c9c0bbe6f50ff6b20c80a3dbbfc4e92
Kevin Bowen <kevin.bowen@gmail.com>
2024-02-23 15:23:00 -0800

    Add nox coverage session. Update reports. Resolves #414

--- noxfile.py
+++ noxfile.py
@@ -5,14 +5,18 @@ import tempfile
 import nox
 
 PYTHON_VERSIONS = ["3.12", "3.11", "3.10"]
-
-nox.options.sessions = "lint", "safety", "tests"
-locations = "accounts", "config", "tasks", "./noxfile.py", "docs/conf.py"
+nox.options.sessions = "lint", "coverage", "safety", "tests"
+locations = (
+    "accounts",
+    "config",
+    "tasks",
+    "./noxfile.py",
+    "docs/conf.py",
+)
 
 
 def install_with_constraints(session, *args, **kwargs):
-    """
-    Install packages constrained by Poetry's lock file.
+    """Install packages constrained by Poetry's lock file.
 
     This function is a wrapper for nox.sessions.install. It
     invokes pip to install packages inside of the session's virtualenv.
@@ -49,6 +53,17 @@ def black(session):
 
 
 @nox.session(python=PYTHON_VERSIONS)
+def coverage(session):
+    """Build JSON coverage report."""
+    install_with_constraints(session, "coverage")
+    session.run("coverage", "run", "-p", "-m", "pytest")
+    session.run("coverage", "combine")
+    session.run("coverage", "report", "-m", "--skip-covered")
+    session.run("coverage", "json", "-o", "htmlcov/coverage.json")
+    session.run("coverage", "html")
+
+
+@nox.session(python=PYTHON_VERSIONS)
 def docs(session):
     """Build the documentation."""
     install_with_constraints(session, "sphinx")
@@ -63,7 +78,7 @@ def lint(session):
         session,
         "ruff",
     )
-    session.run("ruff", "check", *args)
+    session.run("ruff", "check", ".", *args)
 
 
 @nox.session(python=PYTHON_VERSIONS)
